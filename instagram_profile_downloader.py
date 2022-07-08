ec27ef43984b753ce1443ded981ba0a8dbf40e36
Alireza Shirmohammad <alirezashirmohammad55@gmail.com>
2022-07-08 21:47:22 +0430

    Initial commit🏎

---
+++ instagram_profile_downloader.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+from tkinter import *
+
+#  ========================= GUI ===================================================
+#  general settings
+root = Tk()
+root.title('Instagram profile downloader!')
+root.geometry('300x250')
+root.resizable(width=False, height=False)
+
+
+# main loop-----------------------------
+root.mainloop()
